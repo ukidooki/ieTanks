83a0c5a851a9bfe59fbefbb13701bebe8f2422a7
dawidpazik <pazik@student.agh.edu.pl>
2014-11-05 00:26:15 +0100

    Specify bot actions parameters

--- ieTanks-server/src/main/python/bot/action.py
+++ ieTanks-server/src/main/python/bot/action.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class Action:
     """
         This class represents interface of an action.
@@ -5,14 +7,26 @@ class Action:
     """
     def __init__(self):
         pass
+        
+class Direction(Enum):
+    north = 1
+    south = 2
+    west = 3
+    east = 4        
 
 class Shot(Action):
     def __init__(self, angle):
+        """
+            The second parameter is an integer value <1..360>
+        """
         Action.__init__(self)
         self.angle = angle
 
 class Move(Action):
     def __init__(self, direction):
+        """
+            The second parameter is one of the Direction enum values
+        """
         Action.__init__(self)
         self.direction = direction
 
