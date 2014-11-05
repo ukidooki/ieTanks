cc38db2ddbd224301198111b7e26eb48c052ea74
kamilmielnik <kamil.adam.mielnik@gmail.com>
2014-11-05 19:26:35 +0100

    Following python coding style guideline, docstrings corrected

--- ieTanks-server/src/main/python/bot/action.py
+++ ieTanks-server/src/main/python/bot/action.py
@@ -1,35 +1,46 @@
 from enum import Enum
 
+
+class Direction(Enum):
+    north = 1
+    south = 2
+    west = 3
+    east = 4
+
+
 class Action:
     """
         This class represents interface of an action.
         It should be derived from but never instantiated directly.
     """
+
     def __init__(self):
         pass
-        
-class Direction(Enum):
-    north = 1
-    south = 2
-    west = 3
-    east = 4        
+
 
 class Shot(Action):
+
     def __init__(self, angle):
         """
-            The second parameter is an integer value <1..360>
+            Args:
+                angle (float): value in range <0, 2pi>
         """
         Action.__init__(self)
         self.angle = angle
 
+
 class Move(Action):
+
     def __init__(self, direction):
         """
-            The second parameter is one of the Direction enum values
+            Args:
+                direction (Direction): enum value
         """
         Action.__init__(self)
         self.direction = direction
 
+
 class Nothing(Action):
+
     def __init__(self):
         Action.__init__(self)
