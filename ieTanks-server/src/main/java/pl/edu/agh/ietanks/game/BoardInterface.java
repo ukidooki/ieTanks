4d883315859b2d2160a96d623e024745219c2a20
Artur Orzeł <artekorzel@gmail.com>
2014-11-16 21:46:54 +0100

    IETANKS-39: Interfaces changes

--- ieTanks-server/src/main/java/pl/edu/agh/ietanks/game/BoardInterface.java
+++ ieTanks-server/src/main/java/pl/edu/agh/ietanks/game/BoardInterface.java
@@ -1,8 +1,9 @@
 package pl.edu.agh.ietanks.game;
 
+import java.io.Serializable;
 import java.util.List;
 
-public interface BoardInterface {
+public interface BoardInterface extends Serializable {
     public List<TankInterface> getTanks();
     public TankInterface getTank(int id);
     public List<ProjectileInterface> getBullets();
@@ -10,5 +11,4 @@ public interface BoardInterface {
     public void addBullet(ProjectileInterface bullet);
     public int getWidth();
     public int getHeight();
-    public Object getBoard(); // graphical representation of board with all elements
 }
--- ieTanks-server/src/main/java/pl/edu/agh/ietanks/game/ProjectileInterface.java
+++ ieTanks-server/src/main/java/pl/edu/agh/ietanks/game/ProjectileInterface.java
@@ -1,7 +1,7 @@
 package pl.edu.agh.ietanks.game;
 
 public interface ProjectileInterface extends MovableInterface {
-    public int shotBy(); // id of tank that shot the projectile
+    public int getShotBy(); // id of tank that shot the projectile
     public int getDirectionX();
     public int getDirectionY();
 }
