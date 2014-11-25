b1626b39d9c00e87189aabd6c9bfe68ae88f10a6
Igor Jurkowski <igor@student.agh.edu.pl>
2014-11-25 15:18:40 +0100

    [IETANKS-40]Added base for frontend development: 	* decided on Phaser in the end (gives some good abstractions over tiled map and there are tons of tutorials) 	* for now using bower for dependencies (see bower.json file) 	* each squad will create its own angular module which we can then add in app.js to modules seen (and to angularRoute so it sees the partial HTMLs) 	* for now using base Bootstrap css, we can always generate one with custom colours

---
+++ ieTanks-server/src/main/webapp/js/game.js
@@ -0,0 +1,7 @@
+var ieTanksVisualization = angular.module('ieTanksVisualization', []);
+
+ieTanksVisualization.controller('GameCtrl', ['$scope',
+    function($scope) {
+        $scope.placeholderText = "Placeholder"
+    }
+]);
