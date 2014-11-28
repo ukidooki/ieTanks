3f1e2f9e46eea2cdd07bf70139b6891403b44464
Yaroslav Machkivskiy <ihoo.ir@gmail.com>
2014-11-28 19:14:51 +0100

    Use of concurrent map and provide unmodifiable lists in GameRunner getters.

--- ieTanks-gameplay/src/main/java/pl/edu/agh/ietanks/gameplay/game/InMemoryGameHistory.java
+++ ieTanks-gameplay/src/main/java/pl/edu/agh/ietanks/gameplay/game/InMemoryGameHistory.java
@@ -5,54 +5,43 @@ import pl.edu.agh.ietanks.gameplay.game.api.GameHistory;
 import pl.edu.agh.ietanks.gameplay.game.innerapi.GameHistoryStorage;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class InMemoryGameHistory implements GameHistory, GameHistoryStorage {
 
-    private Map<Integer, Game> historyGames = new HashMap<Integer, Game>();
-    private ReentrantLock historyLock = new ReentrantLock();
+    private Map<Integer, Game> historyGames = new ConcurrentHashMap<Integer, Game>();
+    private ReentrantLock gameIdLock = new ReentrantLock();
     private int gameIdCounter = 0;
+
+    private int nextGameId(){
+        try{
+            gameIdLock.lock();
+            return gameIdCounter++;
+        }finally {
+            gameIdLock.unlock();
+        }
+    }
 
     @Override
     public List<Integer> getFinishedGamesIds() {
-        try{
-            historyLock.lock();
+        List<Integer> finishedGameIds = new ArrayList<Integer>();
+        finishedGameIds.addAll(historyGames.keySet());
 
-            List<Integer> finishedGameIds = new ArrayList<Integer>();
-            finishedGameIds.addAll(historyGames.keySet());
-
-            return finishedGameIds;
-
-        }finally {
-            historyLock.unlock();
-        }
+        return finishedGameIds;
     }
 
     @Override
     public Game getGame(int gameId) {
-        try{
-            historyLock.lock();
-
-            return historyGames.get(gameId);
-        }finally {
-            historyLock.unlock();
-        }
+        return historyGames.get(gameId);
     }
 
     @Override
     public int storeFinishedGame(Game game) {
-        try{
-            historyLock.lock();
-
-            int gameId = gameIdCounter++;
-            historyGames.put(gameId, game);
-
-            return gameId;
-        }finally {
-            historyLock.unlock();
-        }
+        int gameId = nextGameId();
+        historyGames.put(gameId, game);
+        return gameId;
     }
 }
