859d95ef6995502169e8eccea725ceacc5ed00aa
Igor Jurkowski <igor@student.agh.edu.pl>
2014-11-28 04:28:21 +0100

    basic logic for moving and creating tanks added    * some small sprites added    * base for state created    * scaling of grid

--- ieTanks-server/src/main/webapp/js/game.js
+++ ieTanks-server/src/main/webapp/js/game.js
@@ -1,53 +1,120 @@
 var ieTanksVisualization = angular.module('ieTanksVisualization', []);
 
-ieTanksVisualization.controller('GameCtrl', ['$scope',
-    function($scope) {
-        $scope.placeholderText = game
+ieTanksVisualization.controller('GameCtrl', ['$scope', '$interval',
+    function ($scope, $interval) {
+        $scope.map = {border: 20, obstacles:[{type:'', x:5, y:10}]};
+
+        var states = [{players:[{id:'blabla', action:'move', x:'10', y:'5'}], missiles:[]},
+            {players:[{id:'blabla', action:'move', x:'6', y:'2'}], missiles:[]},
+            {players:[{id:'blabla', action:'move', x:'15', y:'19'}], missiles:[]},
+            {players:[{id:'blabla', action:'move', x:'0', y:'19'}], missiles:[]}];
+        $interval(function () {
+            var ind = Math.floor(Math.random()*states.length);
+            $scope.state = states[ind];
+        }, 3000);
+
     }
-]);
+])
+    .directive('tankGame', function () {
+        return {
+            scope: {
+                map: '=',
+                state: '='
+            },
+            link: function ($scope) {
+                var gameBorder = 400;
+                var tileSize = 32;
+                var game, scale, scaledGrid, players, isInit;
+                scale=(gameBorder/$scope.gridBorder)/tileSize;
+                scaledGrid = tileSize * scale;
+                isInit = true;
+
+                var Player = function(id, tank, turret, direction) {
+                    this.id = id;
+                    this.tank = tank;
+                    this.turret = turret;
+                    this.direction = direction;
+                };
+
+                Player.prototype.moveTo = function (game, x, y, scale){
+                    game.add.tween(this.tank).to({
+                        x: Math.floor(tileSize*scale)*x,
+                        y: Math.floor(tileSize*scale)*y
+                    }, 250, Phaser.Easing.Quadratic.InOut, true);
+                };
 
 
-var game = new Phaser.Game(320, 320, Phaser.CANVAS, 'tanks-map', { preload: preload, create: create, update: update, render: render });
+                game = new Phaser.Game(gameBorder, gameBorder, Phaser.CANVAS, 'game-window', {}, true);
+
+                game.state.add('animation', {
+                    preload: preload,
+                    create: create });
+
+                function preload() {
+                    //load image which will be used as ground texture
+                    game.load.image('ground', 'assets/scorched_earth.png');
+                    game.load.image('tank', 'assets/tankBase.png');
+                    game.load.image('turret', 'assets/tankTurret.png');
+                    game.load.image('wall', 'assets/tile_backwall.png');
+                }
+
+                function create() {
+                    //Adds background to map (x_pos, y_pos, x_size, y_size) in pixels
+                    game.add.tileSprite(0, 0, game.width, game.height, 'ground');
+                }
+
+                load_game();
+
+                function load_game(map) {
+                    if(map) {
+                        isInit = true;
+                        players = {};
+                        scale = (gameBorder / map["border"]) / tileSize;
+                        scaledGrid = tileSize * scale;
+                        game.state.start('animation');
+                    }
+                }
+
+                function load_state(state) {
+                    if(state) {
+                        var players_states = state["players"];
+                        console.log(state);
+                        console.log(players);
+                        if(isInit) {
+                            console.log("init");
+                            players_states.forEach(function (state) {
+                                var tank = game.add.sprite(state.x * scaledGrid, state.y * scaledGrid, 'tank');
+                                var turret = game.add.sprite(0, 0, 'turret');
+                                tank.addChild(turret);
+                                tank.scale.x = scale;
+                                tank.scale.y = scale;
+                                var col = Phaser.Color.getRandomColor();
+                                tank.tint = col;
+                                turret.tint = col;
+                                players[state.id] = new Player(state.id, tank, turret, 0);
+                            });
+                            isInit=false;
+                        } else {
+                            console.log("update");
+                            players_states.forEach(function (state) {
+                                players[state.id].moveTo(game, state.x, state.y, scale);
+                            });
+                        }
+
+                    }
+                }
+
+                $scope.$watch('map', load_game, true);
+
+                $scope.$watch('state', load_state);
 
 
+                $scope.$on('$destroy', function() {
+                    game.$destroy();
+                });
 
-function preload() {
-
-    //load image which will be used as ground texture
-    game.load.image('ground', 'http://examples.phaser.io/assets/games/tanks/scorched_earth.png');
-
-}
-
-
-
-var map;
-var layer;
+            }
+        }
+    });
 
 
-
-function create() {
-
-    //Creates a blank tilemap
-    map = game.add.tilemap();
-
-    //Adds background to map (x_pos, y_pos, x_size, y_size) in pixels
-    game.add.tileSprite(0, 0, 320, 320, 'ground');
-
-    //  Creates a new blank layer and sets the map dimensions.
-    //  In this case the map is 10x10 tiles in size and the tiles are 32x32 pixels in size each.
-    layer = map.create('level-ground', 10, 10, 32, 32);
-    //  Resize the world
-    layer.resizeWorld();
-
-}
-
-function update() {
-
-    //Add logic on update here
-
-}
-
-function render() {
-
-
-}
