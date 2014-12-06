f4391e95ae13fb50549573ecafc6c50361550465
mequrel <meq@meq.com>
2014-12-07 00:05:32 +0100

    Fixed GameRunnerFactoryTest

--- ieTanks-gameplay/src/test/java/pl/edu/agh/ietanks/gameplay/game/GameRunnerFactoryTest.java
+++ ieTanks-gameplay/src/test/java/pl/edu/agh/ietanks/gameplay/game/GameRunnerFactoryTest.java
@@ -3,6 +3,7 @@ package pl.edu.agh.ietanks.gameplay.game;
 import com.google.common.collect.Lists;
 import org.junit.Test;
 import pl.edu.agh.ietanks.boards.model.Board;
+import pl.edu.agh.ietanks.boards.model.Field;
 import pl.edu.agh.ietanks.engine.api.Engine;
 import pl.edu.agh.ietanks.gameplay.game.api.BotAlgorithm;
 import pl.edu.agh.ietanks.gameplay.game.api.BotId;
@@ -26,8 +27,9 @@ public class GameRunnerFactoryTest {
         BotAlgorithm firstBotAlgorithm = new BotAlgorithm(new BotId("first-bot"), "python");
         BotAlgorithm secondBotAlgorithm = new BotAlgorithm(new BotId("second-bot"), "not-python");
         List<BotAlgorithm> algorithms = Lists.newArrayList(firstBotAlgorithm, secondBotAlgorithm);
+        List<Field> startingPoints = Lists.newArrayList(new Field(0, 0), new Field(2, 3));
 
-        Board board = new Board(1, "board", 3, 4, Collections.emptyList(), Collections.emptyList());
+        Board board = new Board(1, "board", 3, 4, Collections.emptyList(), startingPoints);
 
         // when
         GameRunner runner = factory.create(board, algorithms, mock(Engine.class));
