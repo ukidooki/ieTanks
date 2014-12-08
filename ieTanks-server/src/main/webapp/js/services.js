56c8e0dca99f85442354d1e075a9356d00a84820
Igor Jurkowski <igor@student.agh.edu.pl>
2014-12-08 11:12:30 +0100

    small fix for missing comma

--- ieTanks-server/src/main/webapp/js/services.js
+++ ieTanks-server/src/main/webapp/js/services.js
@@ -6,7 +6,7 @@ ieTanksServices.factory('REST', ['$resource',
             games:  $resource('/api/game'),         // GET
             game:   $resource('/api/game/:gameId'), // GET|POST
             bot:    $resource('/api/bot'),          // GET|POST
-            maps:   $resource('/api/board')         // GET
+            maps:   $resource('/api/board'),      // GET
             auth:   $resource('/api/login')         // POST
             // add more calls to external components
         };
