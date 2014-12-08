0a99eb89101233d52dab64c17c8bbdbc87ef64e4
kamilmielnik <kamil.adam.mielnik@gmail.com>
2014-12-08 23:39:47 +0100

    SimpleBoard class removed, board creation moved into method

--- ieTanks-boards/src/main/java/pl/edu/agh/ietanks/boards/BoardsBean.java
+++ ieTanks-boards/src/main/java/pl/edu/agh/ietanks/boards/BoardsBean.java
@@ -2,21 +2,17 @@ package pl.edu.agh.ietanks.boards;
 
 import org.springframework.stereotype.Service;
 import pl.edu.agh.ietanks.boards.api.BoardsReader;
-import pl.edu.agh.ietanks.boards.impl.SimpleBoard;
 import pl.edu.agh.ietanks.boards.model.Board;
+import pl.edu.agh.ietanks.boards.model.Field;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class BoardsBean implements BoardsReader {
     private Map<Integer, Board> boards = new HashMap<>();
 
     public BoardsBean() {
-        Board simpleBoard = new SimpleBoard().create(1);
-        boards.put(1, simpleBoard);
+        boards.put(1, createSimpleBoard(1));
     }
 
     @Override
@@ -29,4 +25,25 @@ public class BoardsBean implements BoardsReader {
         return boards.get(boardId);
     }
 
+    private Board createSimpleBoard(int id) {
+        int width = 10;
+        int height = 10;
+        List<Field> obstacles = Arrays.asList(
+                new Field(1, 1),
+                new Field(1, 8),
+                new Field(8, 1),
+                new Field(8, 8),
+                new Field(3, 3),
+                new Field(3, 6),
+                new Field(6, 3),
+                new Field(6, 6)
+        );
+        List<Field> startingPoints = Arrays.asList(
+                new Field(0, 0),
+                new Field(0, 9),
+                new Field(9, 0),
+                new Field(9, 9)
+        );
+        return new Board(id, "Simple board", width, height, obstacles, startingPoints);
+    }
 }
